9b3a974479075f54fd3aab78b90dbe3d26d1320f
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-01 02:55:30 +0900

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = "Stop"
 Invoke-WebRequest -Uri "https://github.com/kawanakaiku/test-ci/releases/download/win10/Optimize-Offline.zip" -OutFile "Optimize-Offline.zip" -Verbose -PassThru
 Expand-Archive ".\Optimize-Offline.zip" .
 
-Foreach ($item in @("aa", "ab", "ac", "ad", "ae")) { Invoke-WebRequest -Uri ("https://github.com/kawanakaiku/test-ci/releases/download/win10/Win10_21H2_Japanese_x64.wim_" + $item) -OutFile (Win10_21H2_Japanese_x64.wim_" + $item) -Verbose -PassThru }
+Foreach ($item in @("aa", "ab", "ac", "ad", "ae")) { Invoke-WebRequest -Uri '{0}{1}' -f "https://github.com/kawanakaiku/test-ci/releases/download/win10/Win10_21H2_Japanese_x64.wim_", $item -OutFile '{0}{1}' -f "Win10_21H2_Japanese_x64.wim_", $item -Verbose -PassThru }
 Get-Content "Win10_21H2_Japanese_x64.wim_*" | Set-Content "Win10_21H2_Japanese_x64.wim"
 Remove-Item "Win10_21H2_Japanese_x64.wim_*"
 

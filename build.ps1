43deb9bb0677b53508b087fa36e6dd42a285781b
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-01 04:22:01 +0900

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -10,10 +10,11 @@ $wc = New-Object net.webclient
 $wc.DownloadFile("https://github.com/kawanakaiku/test-ci/releases/download/win10/Optimize-Offline.zip", "Optimize-Offline.zip")
 Expand-Archive ".\Optimize-Offline.zip" .
 
-Foreach ($item in @("aa", "ab", "ac", "ad", "ae")) { Write-Output -InputObject $item ; $wc.DownloadFile("https://github.com/kawanakaiku/test-ci/releases/download/win10/Win10_21H2_Japanese_x64.wim_" + $item, "Win10_21H2_Japanese_x64.wim_" + $item) }
-Get-Content "Win10_21H2_Japanese_x64.wim_*" | Set-Content "Win10_21H2_Japanese_x64.wim"
-Write-Output -InputObject "merging to a wim file"
-Remove-Item "Win10_21H2_Japanese_x64.wim_*"
-Write-Output -InputObject "download completed"
+# Foreach ($item in @("aa", "ab", "ac", "ad", "ae")) { Write-Output -InputObject $item ; $wc.DownloadFile("https://github.com/kawanakaiku/test-ci/releases/download/win10/Win10_21H2_Japanese_x64.wim_" + $item, "Win10_21H2_Japanese_x64.wim_" + $item) }
+# Write-Output -InputObject "merging to a wim file"
+# Get-Content "Win10_21H2_Japanese_x64.wim_*" | Set-Content "Win10_21H2_Japanese_x64.wim"
+# Remove-Item "Win10_21H2_Japanese_x64.wim_*"
+# Write-Output -InputObject "download completed"
+python.exe -c "import io, shutil;from urllib.request import urlopen;b=io.BytesIO();[b.write(urlopen('https://github.com/kawanakaiku/test-ci/releases/download/win10/Win10_21H2_Japanese_x64.wim_'+i).read()) for i in ['aa', 'ab', 'ac', 'ad', 'ae']];b.seek(0);shutil.copyfileobj(b, open('Win10_21H2_Japanese_x64.wim', 'wb'))"
 
 powershell.exe -NoProfile -ExecutionPolicy Unrestricted ".\Start-Optimize.ps1"

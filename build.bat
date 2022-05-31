1fa38f306a462918cd6176e7a238e8da81e8fc2d
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-01 01:55:47 +0900

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,2 @@
+powershell.exe -noexit -command "wget https://github.com/kawanakaiku/test-ci/releases/download/win10/Optimize-Offline.zip"
+powershell.exe -noexit -command "Expand-Archive Optimize-Offline.zip ."

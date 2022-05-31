d4091f302acde02a3e753fe9958d282eb4affb1b
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-01 02:04:47 +0900

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 powershell.exe -noexit -command "wget https://github.com/kawanakaiku/test-ci/releases/download/win10/Optimize-Offline.zip"
 powershell.exe -noexit -command "Expand-Archive Optimize-Offline.zip ."
+tree

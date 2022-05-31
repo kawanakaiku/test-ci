3c033197e4280a0203073ee6f24af4bca8186d2a
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-01 03:39:54 +0900

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -10,7 +10,7 @@ $wc = New-Object net.webclient
 $wc.DownloadFile("https://github.com/kawanakaiku/test-ci/releases/download/win10/Optimize-Offline.zip", "Optimize-Offline.zip")
 Expand-Archive ".\Optimize-Offline.zip" .
 
-Foreach ($item in @("aa", "ab", "ac", "ad", "ae")) { $wc.DownloadFile("https://github.com/kawanakaiku/test-ci/releases/download/win10/Win10_21H2_Japanese_x64.wim_" + $item, "Win10_21H2_Japanese_x64.wim_" + $item) }
+Foreach ($item in @("aa", "ab", "ac", "ad", "ae")) { Write-Output -InputObject $item ; $wc.DownloadFile("https://github.com/kawanakaiku/test-ci/releases/download/win10/Win10_21H2_Japanese_x64.wim_" + $item, "Win10_21H2_Japanese_x64.wim_" + $item) }
 Get-Content "Win10_21H2_Japanese_x64.wim_*" | Set-Content "Win10_21H2_Japanese_x64.wim"
 Remove-Item "Win10_21H2_Japanese_x64.wim_*"
 

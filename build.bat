877dfd36e1a9ecc5f4a650d6e215950fb18628fe
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-01 02:14:24 +0900

    Update and rename build.bat to build.ps1

--- build.bat
+++
@@ -1,3 +0,0 @@
-powershell.exe -noexit -command "wget https://github.com/kawanakaiku/test-ci/releases/download/win10/Optimize-Offline.zip"
-powershell.exe -noexit -command "Expand-Archive Optimize-Offline.zip ."
-tree
---
+++ build.ps1
@@ -0,0 +1,7 @@
+wget https://github.com/kawanakaiku/test-ci/releases/download/win10/Optimize-Offline.zip
+Expand-Archive Optimize-Offline.zip .
+
+#https://github.com/kawanakaiku/test-ci/releases/download/win10/Win10_21H2_Japanese_x64.wim_aa
+
+python.exe --version
+python --version

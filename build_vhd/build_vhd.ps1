46ad5e7b0eee5ddc9a87e8faf6ae68efd291819e
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-01 20:25:06 +0900

    Update build_vhd.ps1

--- build_vhd/build_vhd.ps1
+++ build_vhd/build_vhd.ps1
@@ -1,3 +1,9 @@
+# exit on fail
+$ErrorActionPreference = "Stop"
+# no progress bar for faster download
+# https://stackoverflow.com/questions/28682642/powershell-why-is-using-invoke-webrequest-much-slower-than-a-browser-download
+$ProgressPreference = "SilentlyContinue"
+
 Write-Output -InputObject "download starting"
 # python.exe -c "from urllib.request import urlopen;f=open('Win10_21H2_Japanese_x64.esd', 'wb');[[print(i), f.write(urlopen('https://github.com/kawanakaiku/test-ci/releases/download/win10_custom/Win10_21H2_Japanese_x64.esd_'+i).read())] for i in ['aa', 'ab']];f.close()"
 Write-Output -InputObject "download completed"

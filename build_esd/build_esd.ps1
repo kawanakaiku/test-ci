80fda912af7402367ee5a3831205f853fdc72b63
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-01 14:32:38 +0900

    Create build_esd.ps1

---
+++ build_esd/build_esd.ps1
@@ -0,0 +1,26 @@
+	# exit on fail
+# $ErrorActionPreference = "Stop"
+# no progress bar for faster download
+# https://stackoverflow.com/questions/28682642/powershell-why-is-using-invoke-webrequest-much-slower-than-a-browser-download
+# $ProgressPreference = "SilentlyContinue"
+
+# client for download
+$wc = New-Object net.webclient
+
+$wc.DownloadFile("https://github.com/kawanakaiku/test-ci/releases/download/win10/Optimize-Offline.zip", "Optimize-Offline.zip")
+Expand-Archive ".\Optimize-Offline.zip" .
+
+Write-Output -InputObject "download starting"
+# Foreach ($item in @("aa", "ab", "ac", "ad", "ae")) { Write-Output -InputObject $item ; $wc.DownloadFile("https://github.com/kawanakaiku/test-ci/releases/download/win10/Win10_21H2_Japanese_x64.wim_" + $item, "Win10_21H2_Japanese_x64.wim_" + $item) }
+# Write-Output -InputObject "merging to a wim file"
+# Get-Content "Win10_21H2_Japanese_x64.wim_*" | Set-Content "Win10_21H2_Japanese_x64.wim"
+# Remove-Item "Win10_21H2_Japanese_x64.wim_*"
+python.exe -c "from urllib.request import urlopen;f=open('Win10_21H2_Japanese_x64.wim', 'wb');[[print(i), f.write(urlopen('https://github.com/kawanakaiku/test-ci/releases/download/win10/Win10_21H2_Japanese_x64.wim_'+i).read())] for i in ['aa', 'ab', 'ac', 'ad', 'ae']];f.close()"
+Write-Output -InputObject "download completed"
+
+powershell.exe -NoProfile -ExecutionPolicy Unrestricted ".\Start-Optimize.ps1"
+
+# store logs
+# Compress-Archive -Path .\Optimize-Offline_*\*.log, .\Optimize-Offline_*\*\*.log -DestinationPath logs.zip -Force
+
+cmd.exe /c "dir"

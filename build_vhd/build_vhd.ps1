558e10856a7f28fd5ef3a4d870a1ab63b5bf9814
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-01 21:36:30 +0900

    Update build_vhd.ps1

--- build_vhd/build_vhd.ps1
+++ build_vhd/build_vhd.ps1
@@ -8,6 +8,8 @@ Write-Output -InputObject "download starting"
 # python.exe -c "from urllib.request import urlopen;f=open('Win10_21H2_Japanese_x64.esd', 'wb');[[print(i), f.write(urlopen('https://github.com/kawanakaiku/test-ci/releases/download/win10_custom/Win10_21H2_Japanese_x64.esd_'+i).read())] for i in ['aa', 'ab']];f.close()"
 Write-Output -InputObject "download completed"
 
+Write-Output -InputObject "enable hyper-v"
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 
 # create vhd file
 New-VHD -Path .\win10.vhd -SizeBytes 32GB -Dynamic

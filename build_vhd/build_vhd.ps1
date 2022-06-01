faa88b32cf58186615a2e45805319084a552bf7b
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-01 20:23:01 +0900

    Update build_vhd.ps1

--- build_vhd/build_vhd.ps1
+++ build_vhd/build_vhd.ps1
@@ -2,6 +2,7 @@ Write-Output -InputObject "download starting"
 # python.exe -c "from urllib.request import urlopen;f=open('Win10_21H2_Japanese_x64.esd', 'wb');[[print(i), f.write(urlopen('https://github.com/kawanakaiku/test-ci/releases/download/win10_custom/Win10_21H2_Japanese_x64.esd_'+i).read())] for i in ['aa', 'ab']];f.close()"
 Write-Output -InputObject "download completed"
 
+
 # create vhd file
 New-VHD -Path .\win10.vhd -SizeBytes 32GB -Dynamic
 

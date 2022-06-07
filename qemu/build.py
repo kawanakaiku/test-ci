2061e30b498bde0fc13e5a09583fb58cd5e2f93c
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-07 15:06:12 +0900

    Update build.py

--- qemu/build.py
+++ qemu/build.py
@@ -14,7 +14,7 @@ GUEST_OUT = 'guest.out'
 os.mkfifo(GUEST_IN)
 os.mkfifo(GUEST_OUT)
 
-os.system("qemu-system-x86_64 -serial pipe:guest -machine q35 -m 1024 -smp cpus=2 -cpu qemu64 -drive if=pflash,format=raw,read-only,file=edk2-x86_64-code.fd -netdev user,id=n1,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 -cdrom alpine-virt-3.16.0-x86_64.iso -nographic &")
+os.system(f"qemu-system-x86_64 -machine q35 -m 1024 -smp cpus=2 -cpu qemu64 -drive if=pflash,format=raw,read-only,file=edk2-x86_64-code.fd -netdev user,id=n1,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 -cdrom alpine-virt-3.16.0-x86_64.iso -nographic <{GUEST_IN} >{GUEST_OUT} &")
 
 print("Opening FIFO...")
 with open(GUEST_IN, 'rb') as stdin, open(GUEST_OUT, 'rb') as stdout:

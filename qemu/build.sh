af194899aa89a5f0e65c1d1596a20579b73af5e0
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-07 15:28:53 +0900

    Update build.sh

--- qemu/build.sh
+++ qemu/build.sh
@@ -1,7 +1,13 @@
+#sudo apt-get install -y --no-install-recommends qemu-system-x86 qemu-utils
+#qemu-system-x86_64 --version
+#wget -nc -nv https://github.com/kawanakaiku/test-ci/releases/download/files/edk2-x86_64-code.fd
+#wget -nc -nv https://dl-cdn.alpinelinux.org/alpine/v3.16/releases/x86_64/alpine-virt-3.16.0-x86_64.iso
+
 mkfifo guest.in guest.out
 
 echo starting qemu
-qemu-system-x86_64 -machine q35 -m 1024 -smp cpus=2 -cpu qemu64 -drive if=pflash,format=raw,read-only,file=edk2-x86_64-code.fd -netdev user,id=n1,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 -cdrom alpine-virt-3.16.0-x86_64.iso -nographic < guest.in > guest.out &
+#qemu-system-x86_64 -machine q35 -m 1024 -smp cpus=2 -cpu qemu64 -drive if=pflash,format=raw,read-only,file=edk2-x86_64-code.fd -netdev user,id=n1,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 -cdrom alpine-virt-3.16.0-x86_64.iso -nographic < guest.in > guest.out &
+bash -c 'for i in {1..10} ; do echo "counting $i..."' < guest.in > guest.out &
 echo starting qemu finished
 
 echo reading stdout

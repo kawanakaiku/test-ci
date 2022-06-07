add963ac4f272b47497a2348f63b27ed738adb6f
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-07 20:15:14 +0900

    Update build.py

--- qemu/build.py
+++ qemu/build.py
@@ -6,23 +6,20 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-# start qemu process
-logger.info("starting qemu")
+def start_qemu(cdrom=True):
+    global process
+    logger.info("starting qemu")
+    command = ["qemu-system-x86_64"]
+    command += "-machine q35 -m 2048 -smp cpus=2 -cpu qemu64 -nographic".split()
+    command += " -drive if=pflash,format=raw,read-only,file=edk2-x86_64-code.fd".split()
+    command += "-netdev user,id=n1,hostfwd=tcp::2222-:22".split()
+    command += "-device virtio-net,netdev=n1".split()
+    if cdrom:
+        command += "-cdrom alpine-virt-3.16.0-x86_64.iso".split()
+    command += ["qemu.img"]
+    process = Popen(command, stdin=PIPE, stderr=DEVNULL, text=True, encoding="utf-8", errors="ignore")
+    logger.info("started qemu in background")
 
-command = ["qemu-system-x86_64"]
-command += "-machine q35 -m 2048 -smp cpus=2 -cpu qemu64 -nographic".split()
-command += " -drive if=pflash,format=raw,read-only,file=edk2-x86_64-code.fd".split()
-command += "-netdev user,id=n1,hostfwd=tcp::2222-:22".split()
-command += "-device virtio-net,netdev=n1".split()
-command += "-cdrom alpine-virt-3.16.0-x86_64.iso".split()
-command += ["qemu.img"]
-
-process = Popen(command, stdin=PIPE, stderr=DEVNULL, text=True, encoding="utf-8", errors="ignore")
-
-logger.info("started qemu in background")
-
-
-# communicate with qemu
 def write(message):
     logger.info(f"writing '{message}'")
     process.stdin.write(message + "\n")
@@ -31,6 +28,9 @@ def write(message):
 def wait_shutdown():    
     process.wait()
 
+
+start_qemu(cdrom=True)
+    
 write("")
 sleep(100)
 
@@ -69,6 +69,39 @@ write("")
 sleep(5)
 write("y")
 sleep(10)
+
+write("poweroff")
+wait_shutdown()
+
+
+
+start_qemu(cdrom=False)
+
+write("")
+sleep(100)
+
+write("root")
+sleep(3)
+
+write(r"""sed -i -E 's/relatime/noatime/g ; s/[0-9]+$/0/g ; s/(\s+?ext4\s+?\S+)/\1,barrier=0/g' /etc/fstab""")
+sleep(3)
+
+write("rm /etc/motd")
+sleep(3)
+
+write(r"""sed -i -E 's/GRUB_TIMEOUT=\S+?/GRUB_TIMEOUT=0/ ; s/(GRUB_CMDLINE_LINUX_DEFAULT=".*?)"/\1 mitigations=off fsck.mode=skip nowatchdog selinux=0 apparmor=0 lsm="/' /etc/default/grub""")
+
+write("grub-mkconfig -o /boot/grub/grub.cfg")
+sleep(10)
+
+write("apk update && apk --no-cache add docker htop ncdu ; service docker start ; rc-update add docker ; apk cache clean")
+sleep(30)
+
+write("reboot")
+sleep(100)
+
+write("root")
+sleep(100)
 
 write("poweroff")
 wait_shutdown()

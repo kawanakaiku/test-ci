fd973a43618745529a2450fa8430400049b4073c
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-07 16:03:45 +0900

    Update build.py

--- qemu/build.py
+++ qemu/build.py
@@ -23,7 +23,7 @@ logger.info("started qemu in background")
 
 # communicate with qemu
 def read():
-    return process.stdout.readline().strip()
+    return process.stdout.readline()
 
 def write(message):
     process.stdin.write(message + "\n")
@@ -33,8 +33,10 @@ logger.info("Opening FIFO...")
 write("")
 while True:
     data = read()
-    if len(data) == 0:
+    if data == "":
         print("Writer closed")
         break
-    logger.info('>>>' + data)
+    data = data.strip()
+    if data != "":
+        logger.info('>>>' + data)
     

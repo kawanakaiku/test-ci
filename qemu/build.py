4fecd927b15e1eef2290835593b712bf2b61098b
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-07 14:58:41 +0900

    Update build.py

--- qemu/build.py
+++ qemu/build.py
@@ -8,17 +8,18 @@ logger = logging.getLogger(__name__)
 import os
 import errno
 
-FIFO = 'guest'
+GUEST_IN = 'guest.in'
+GUEST_OUT = 'guest.out'
 
 os.mkfifo(FIFO)
 
 os.system("qemu-system-x86_64 -serial pipe:guest -machine q35 -m 1024 -smp cpus=2 -cpu qemu64 -drive if=pflash,format=raw,read-only,file=edk2-x86_64-code.fd -netdev user,id=n1,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 -cdrom alpine-virt-3.16.0-x86_64.iso -nographic &")
 
 print("Opening FIFO...")
-with open(FIFO, 'rb') as fifo:
-    print("FIFO opened")
+with open(GUEST_IN, 'rb') as in, open(GUEST_OUT, 'rb') as out:
+    print("in/out opened")
     while True:
-        data = fifo.readline()
+        data = out.readline()
         if len(data) == 0:
             print("Writer closed")
             break

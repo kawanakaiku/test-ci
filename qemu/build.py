3e3f77a5d1cf6710d3efdc5e14bb430fffac3fc2
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-07 15:03:35 +0900

    Update build.py

--- qemu/build.py
+++ qemu/build.py
@@ -11,7 +11,8 @@ import errno
 GUEST_IN = 'guest.in'
 GUEST_OUT = 'guest.out'
 
-os.mkfifo(FIFO)
+os.mkfifo(GUEST_IN)
+os.mkfifo(GUEST_OUT)
 
 os.system("qemu-system-x86_64 -serial pipe:guest -machine q35 -m 1024 -smp cpus=2 -cpu qemu64 -drive if=pflash,format=raw,read-only,file=edk2-x86_64-code.fd -netdev user,id=n1,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 -cdrom alpine-virt-3.16.0-x86_64.iso -nographic &")
 

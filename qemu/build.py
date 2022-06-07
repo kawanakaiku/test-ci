728091ce1b5ae1f1576029d3662020b209afdb76
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-07 14:51:54 +0900

    Update build.py

--- qemu/build.py
+++ qemu/build.py
@@ -15,10 +15,12 @@ os.mkfifo(FIFO)
 os.system("qemu-system-x86_64 -serial pipe:guest -machine q35 -m 1024 -smp cpus=2 -cpu qemu64 -drive if=pflash,format=raw,read-only,file=edk2-x86_64-code.fd -netdev user,id=n1,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 -cdrom alpine-virt-3.16.0-x86_64.iso -nographic &")
 
 print("Opening FIFO...")
-with open(FIFO) as fifo:
+with open(FIFO, 'rb') as fifo:
     print("FIFO opened")
     while True:
         data = fifo.readline()
         if len(data) == 0:
             print("Writer closed")
             break
+        print(data.decode('utf-8', errors='ignore'))
+    

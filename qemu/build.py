7639dd6f806f6214c3bca694333d3a5ced6f4ea7
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-07 18:36:46 +0900

    Update build.py

--- qemu/build.py
+++ qemu/build.py
@@ -1,4 +1,4 @@
-from subprocess import Popen, PIPE, DEVNULL
+from subprocess import run, PIPE, DEVNULL
 from time import sleep
 
 import logging
@@ -17,47 +17,23 @@ command += "-device virtio-net,netdev=n1".split()
 command += "-cdrom alpine-virt-3.16.0-x86_64.iso".split()
 command += ["qemu.img"]
 
-process = Popen(command, stdin=PIPE, stdout=PIPE, stderr=DEVNULL, text=True, encoding="utf-8", errors="ignore")
+process = run(command, stdin=PIPE, stderr=DEVNULL, text=True, encoding="utf-8", errors="ignore")
 
 logger.info("started qemu in background")
 
 
 # communicate with qemu
-def read():
-    return process.stdout.readline()
-
 def write(message):
     process.stdin.write(message + "\n")
     process.stdin.flush()
 
-def answer(pattern, message):    
-    while True:
-        data = read()
-        if data == "":
-            print("Writer closed")
-            break
-        data = data.strip()
-        if data != "":
-            logger.info('<<< ' + data)
-        if pattern in data:
-            sleep(1)
-            write(message)
-            logger.info('>>> ' + message)
-            break
-
 def wait_shutdown():    
-    while True:
-        data = read()
-        if data == "":
-            return
-        data = data.rstrip()
-        if data != "":
-            logger.info('<<< ' + data)
+    process.wait()
 
-logger.info("Opening FIFO...")
 write("")
+sleep(60)
 
-answer("Welcome to Alpine", "root")
+write("root")
 sleep(3)
 
 write("setup-interfaces")

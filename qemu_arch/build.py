ae20e4ff34eb57234929c687509b848c88ea6d16
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-08 10:59:18 +0900

    Update build.py

--- qemu_arch/build.py
+++ qemu_arch/build.py
@@ -33,7 +33,7 @@ start_qemu(cdrom=True)
 # edit boot param
 write(); sleep(10)
 write("\t", end=""); sleep(3)
-write(" console=tty0"); sleep(100)
+write(" console=tty0 console=ttyS0,115200"); sleep(100)
 
 write("root"); sleep(30)
 

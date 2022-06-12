87fbcde4a2fff728d91e4d6f14a989473cd36ca9
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-13 00:14:18 +0900

    Update main.sh

--- test_ubuntu/main.sh
+++ test_ubuntu/main.sh
@@ -4,7 +4,8 @@ echo 'Acquire::Languages "none";' | sudo tee /etc/apt/apt.conf.d/99_translations
 # sudo dpkg --add-architecture i386
 echo sources.list: ; sudo cat /etc/apt/sources.list || true
 echo sources.list.d ; sudo find /etc/apt/sources.list.d
-echo 'deb http://archive.ubuntu.com/ubuntu/ jammy main restricted universe multiverse' | sudo tee /etc/apt/sources.listecho "path-exclude=/usr/share/man/*" | sudo tee /etc/dpkg/dpkg.cfg.d/01_nodoc
+echo 'deb http://archive.ubuntu.com/ubuntu/ jammy main restricted universe multiverse' | sudo tee /etc/apt/sources.list
+sudo rm -rf /etc/apt/sources.list.d
 
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends gcc-multilib gcc-i686-linux-gnu

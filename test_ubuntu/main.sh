27bb8fe3fbf4b18077bf2e50e4d2972f9bc960c5
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-13 00:28:45 +0900

    Update main.sh

--- test_ubuntu/main.sh
+++ test_ubuntu/main.sh
@@ -11,4 +11,4 @@ sudo rm -rf /etc/apt/sources.list.d
 sudo apt-get update
 sudo apt-get install -y gcc-multilib gcc-i686-linux-gnu
 
-i686-linux-gnu-gcc -s -pipe -O3 $(dirname $0)/primes.c -o primes
+i686-linux-gnu-gcc -m32 -s -pipe -O3 $(dirname $0)/primes.c -o primes

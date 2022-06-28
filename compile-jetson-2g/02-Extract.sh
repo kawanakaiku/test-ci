92d4c4b58b03fa63abb95b962e8ea6e88e865f0a
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-28 22:38:01 +0900

    Update 02-Extract.sh

--- compile-jetson-2g/02-Extract.sh
+++ compile-jetson-2g/02-Extract.sh
@@ -1,2 +1,9 @@
-unzip image.zip  # extracts "sd-blob.img"
-rm image.zip
+# unzip image.zip  # extracts "sd-blob.img"
+# rm image.zip
+
+sudo apt-get -o Acquire::Languages=none update
+sudo apt-get install -y avfs
+
+mountavfs
+
+ln -s ${HOME}/.avfs/${PWD}/image.zip#/sd-blob.img sd-blob.img

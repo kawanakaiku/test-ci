5802dc2334d718c54fe27a09fd281b449602085d
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-06-28 22:42:06 +0900

    Update 02-Extract.sh

--- compile-jetson-2g/02-Extract.sh
+++ compile-jetson-2g/02-Extract.sh
@@ -1,5 +1,10 @@
-# unzip image.zip  # extracts "sd-blob.img"
-# rm image.zip
+unzip image.zip  # extracts "sd-blob.img"
+rm image.zip
+
+exit 0
+
+
+# Error 38 when determining sector size! Setting sector size to 512
 
 sudo apt-get -o Acquire::Languages=none update
 sudo apt-get install -y avfs

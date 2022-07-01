1d288de93b61724106d720331e8dc123521c971a
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-07-01 22:55:21 +0900

    Update 09-Compile.sh

--- compile-jetson-2g/09-Compile.sh
+++ compile-jetson-2g/09-Compile.sh
@@ -3,7 +3,7 @@ sudo chroot mnt /bin/bash <<'chroot_end'
 export LDFLAGS+=" -L/lib -L/usr/local/cuda/lib64 -L/usr/lib -L/usr/lib/aarch64-linux-gnu -L/lib/aarch64-linux-gnu"
 export CFLAGS+=" -mcpu=cortex-a57 -I/include -I/usr/local/cuda/include -I/usr/include -I/usr/include/aarch64-linux-gnu"
 export CXXFLAGS+=" -mcpu=cortex-a57 -I/include -I/usr/local/cuda/include -I/usr/include -I/usr/include/aarch64-linux-gnu"
-export MAKEFLAGS=-j1
+#export MAKEFLAGS=-j1
 
 export NVCC=/usr/local/cuda/bin/nvcc
 export CUDACXX=/usr/local/cuda/bin/nvcc
@@ -12,7 +12,7 @@ export USE_OPENCV=1 USE_BLAS=openblas USE_CUDA=1 USE_CUDA_PATH=/usr/local/cuda U
 export CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda/targets/aarch64-linux
 export CUDA_HOME=/usr/local/cuda
 
-export MAX_JOBS=2
+export MAX_JOBS=4
 
 cd /src
 

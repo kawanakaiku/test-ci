505635349f855fe72d5543d8c571dd66b7ac6a1c
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-07-03 18:29:41 +0900

    Update mod_apt.sh

--- mod_apt.sh
+++ mod_apt.sh
@@ -1,6 +1,16 @@
+VERSION_CODENAME=$( . /etc/os-release ; echo $VERSION_CODENAME )
+
 sudo rm -f /etc/apt/apt.conf.d/*
+
 echo 'path-exclude=/usr/share/man/*' | sudo tee /etc/dpkg/dpkg.cfg.d/01_nodoc
 echo 'Acquire::Languages "none";' | sudo tee /etc/apt/apt.conf.d/99_translations
 
 sudo rm -f /etc/apt/sources.list.d/*
-echo "deb http://azure.archive.ubuntu.com/ubuntu/ $( . /etc/os-release ; echo $VERSION_CODENAME ) main restricted universe multiverse" | sudo tee /etc/apt/sources.list
+
+cat <<LIST | sudo tee /etc/apt/sources.list >/dev/null
+$(
+  for dist in ${VERSION_CODENAME}{,-{backports,security,updates}} ; do
+    echo "deb http://azure.archive.ubuntu.com/ubuntu/ ${dist} main restricted universe multiverse"
+  done
+)
+LIST

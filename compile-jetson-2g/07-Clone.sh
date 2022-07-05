a069ad904e4a5a3ca52977dae006a17db5edd78b
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-07-05 20:53:38 +0900

    Update 07-Clone.sh

--- compile-jetson-2g/07-Clone.sh
+++ compile-jetson-2g/07-Clone.sh
@@ -1,4 +1,4 @@
-if true ; then
+if ; then
 
 sudo git clone --depth=1 --recursive --branch=v1.11.0 https://github.com/pytorch/pytorch.git src_lower  # should be owned by root
 
@@ -8,13 +8,6 @@ mkdir src_lower
 wget -nv https://github.com/kawanakaiku/test-ci/releases/download/archive/pytorch-v1.11.0.squashfs
 sudo mount -t squashfs pytorch-v1.11.0.squashfs ./src_lower
 
-(
-  cd src
-  sudo git config --global user.name "xxxx"
-  sudo git config --global user.email "xxxxxx@gmail.com"
-  sudo git init
-)
-
 fi
 
 mkdir -p src src_upper src_work
@@ -22,3 +15,10 @@ sudo mount -t overlay overlay -o lowerdir=src_lower,upperdir=src_upper,workdir=s
 
 sudo mkdir -p mnt/src
 sudo mount -o bind src mnt/src
+
+
+sudo chroot mnt /bin/bash <<'chroot_end'
+git config --global user.name "xxxx"
+git config --global user.email "xxxxxx@gmail.com"
+git config --global --add safe.directory /src
+chroot_end

8a2bd6fbfa24e425847fc41fbcc668c17f010b62
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-07-12 16:16:57 +0900

    Update 03-InstallQEMU.sh

--- compile-jetson-2g/03-InstallQEMU.sh
+++ compile-jetson-2g/03-InstallQEMU.sh
@@ -1,2 +1,2 @@
 sudo apt-get -o Acquire::Languages=none update
-sudo apt-get install -y --no-install-recommends arch-test qemu-user-static
+sudo apt-get install -y --no-install-recommends arch-test qemu-user-static mmdebstrap
